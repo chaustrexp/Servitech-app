1ed09b2712f8bbf7c9e011a568dd61cf24b3dedc
chaustrexp <cristianchaustre90@gmail.com>
2026-07-24 11:03:27 -0500

    Agregamiento de plantillas y conexion

--- turnos/urls.py
+++ turnos/urls.py
@@ -1,2 +1,10 @@
 # Las rutas de las páginas
 
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('turno/<str:codigo_turno>/', views.ver_turno, name='ver_turno'),
+    path('turno/<int:turno_id>/retraso/', views.notificar_retraso, name='notificar_retraso'),
+]
