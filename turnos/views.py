7ee3cd4332ed0da77b23e1b08dd1a2041d3cf1a5
chaustrexp <cristianchaustre90@gmail.com>
2026-07-24 11:03:27 -0500

    Agregamiento de plantillas y conexion

--- turnos/views.py
+++ turnos/views.py
@@ -1,3 +1,28 @@
 # Las vistas son la interface con el usuario
 
 # La logica que procesa los datos para las interfaz
+
+
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Turno
+
+# Muestra la interfaz del Turno Digital
+def ver_turno(request, codigo_turno):
+    turno = get_object_or_404(Turno, codigo_turno=codigo_turno)
+    return render(request, 'turnos/turno_digital.html', {'turno': turno})
+
+# Acción para notificar retraso (RF-04 con autenticación)
+@login_required
+def notificar_retraso(request, turno_id):
+    if request.method == "POST":
+        turno = get_object_or_404(Turno, id=turno_id, cliente=request.user)
+        
+        # Validar RN-03 (Límite de aviso único)
+        if not turno.notifico_retraso:
+            turno.notifico_retraso = True
+            turno.minutos_retraso_notificados = 10
+            turno.estado = 'RETRASADO_CON_AVISO'
+            turno.save()
+            
+    return redirect('ver_turno', codigo_turno=turno.codigo_turno)
